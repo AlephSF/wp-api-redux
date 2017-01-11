261cff62c0b684060d128e9a09dd629fca2cc88c
Matt Glaser <matt@jmpdv.com>
2017-01-11 10:05:36 -0800

    adds menus and title/tagline to endpoint

--- includes/endpoints/class-wp-api-redux-data-site.php
+++ includes/endpoints/class-wp-api-redux-data-site.php
@@ -39,9 +39,40 @@ class Wp_Api_Redux_Data_Site extends WP_REST_Controller {
 
 	 public function get_site_data() {
 		 $output = array(
-			 'foo' => 'bar'
+			 'name' => get_bloginfo('name'),
+			 'tagline' => get_bloginfo('description'),
+			 'menus' => $this->get_all_menus()
 		 );
 		 return new WP_REST_Response( $output, 200 );
+	 }
+
+	 public function get_all_menus(){
+		 $locations =  get_registered_nav_menus();
+		 $menus = array();
+		 $loc_index = get_nav_menu_locations();
+		 foreach ($locations as $slug => $location) {
+		 		$menus[$slug] = array(
+					'menu_name' => $location
+				);
+        $menu_items_raw = wp_get_nav_menu_items($loc_index[$slug]);
+				$menu_items = array();
+				foreach ($menu_items_raw as $key => $item) {
+					$menu_items[$key] = array(
+						'id' => $item->ID,
+						'menu_item_parent' => $item->menu_item_parent,
+						'type' => $item->object,
+						'url' => $item->url,
+						'title' => $item->title,
+						'target' => $item->target,
+						'attr_title' => $item->attr_title,
+						'description' => $item->description,
+						'classes' => $item->classes
+					);
+				}
+				$menus[$slug]['menu_items'] = $menu_items;
+		 }
+
+		 return $menus;
 	 }
 
 }
