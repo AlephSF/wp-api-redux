cdee9f8730687cca9aaf98a77e4fa5c958073088
Matt Glaser <matt@jmpdv.com>
2017-01-19 16:08:14 -0800

    adds route property to nav items

--- includes/endpoints/class-wp-api-redux-data-site.php
+++ includes/endpoints/class-wp-api-redux-data-site.php
@@ -58,11 +58,20 @@ class Wp_Api_Redux_Data_Site extends WP_REST_Controller {
         $menu_items_raw = wp_get_nav_menu_items($loc_index[$slug]);
 				$menu_items = array();
 				foreach ($menu_items_raw as $key => $item) {
+
+					$url = parse_url($item->url);
+					// get slug
+					if( strpos( $item->url, get_home_url() ) !== false ) {
+						$route = untrailingslashit( $url['path'] );
+					} else {
+						$route = false;
+					}
 					$menu_items[$key] = array(
 						'id' => $item->ID,
 						'menu_item_parent' => $item->menu_item_parent,
 						'type' => $item->object,
 						'url' => $item->url,
+						'route' => $route,
 						'title' => $item->title,
 						'target' => $item->target,
 						'attr_title' => $item->attr_title,
